1c102c593942ebd83f7a21999bd52cb79e044136
Herverson <herverson.sousa@gmail.com>
2017-06-18 18:03:22 -0300

    Cabeçalho sudoku

---
+++ sudoku.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int soluSudoku(int, int);
+void printSudoku();
+int mesmaColuna(int, int, int);
+int mesmaLinha(int, int, int);
+int mesmoQuadrado(int, int, int);
