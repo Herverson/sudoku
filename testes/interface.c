c1872949821102c01359b983fd0ce465428ceaac
Herverson <herverson.sousa@gmail.com>
2017-06-20 00:22:23 -0300

    novas

--- testes/interface.c
+++ testes/interface.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-  int n = 1, n2 = 1, num = 1, i, j, cont = 0, k = 0;
+  int n = 0, n2 = 0, num = 1, i, j, cont = 0, k = 0;
   int p1[81], p2[81];
   int total = 0;
   int sudoku[9][9]={0,0,0, 0,0,0, 0,1,0,
@@ -58,17 +58,20 @@ int main()
     }
 
   }
+  // loop para receber as coordenadas
   while (num != 0)
   {
     printf("Informe as coordenadas\n");
     scanf("%d", &n);
     scanf("%d", &n2);
+    // saber se ja existe na posição incial
     cont = 0;
     for (i = 0; i < k ; i++)
     {
       if (n == p1[i] && n2 == p2[i])
         cont = 1;
     }
+    // caso não esteja
     if (cont != 1)
     {
       printf("Informe o número\n");
