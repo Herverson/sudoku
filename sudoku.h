d20692cd6030edcd04376c6c89da673218c6fdb5
Herverson <herverson.sousa@gmail.com>
2017-06-21 02:45:39 -0300

    Inicializadas novas funções

--- sudoku.h
+++ sudoku.h
@@ -5,6 +5,10 @@
 int soluSudoku(int, int);
 void geraMatriz();
 void printSudoku();
+void printSudoku2();
+void jogarSudoku();
+void geraMatrizUsuario();
+void verificaSudoku();
 int mesmaColuna(int, int, int);
 int mesmaLinha(int, int, int);
 int mesmoQuadrado(int, int, int);
