7a37857b07f0fba1f70c47e1c738d3125f7793b7
Herverson <herverson.sousa@gmail.com>
2017-06-25 13:53:18 -0300

    Ordenação do top5

--- testes/arquivo.c
+++ testes/arquivo.c
@@ -2,41 +2,35 @@
 #include <stdlib.h>
 int main(int argc, char const *argv[]) {
   FILE *fp;
-  int vetor[5], i, j, vetor2[5];
+  int vetor[6], i, j, pt = 4, aux;
   // abrir arquivo
-  fp = fopen("sudoku.txt","rt");
+  vetor[5] = pt;
+  fp = fopen("top5.txt","rt");
   if(fp == NULL)
   {
     printf("Não foi possivel abrir arquivo\n");
     return 0;
-  }
-  // receber valores
-  for (int i = 0; i < 5; i++)
-  {
-    scanf("%d", &vetor2[i]);
   }
   // ler os dados e colocar na  matriz
   for (i = 0; i < 5; i++)
   {
     fscanf(fp, "%d", &vetor[i]);
   }
-  for (i = 0; i < 5; i++)
+  // ordenar o TOP5
+  for (i = 0; i < 6; i++)
   {
-    for (j = 0; j < 5; j++)
+    for (j = i + 1; j < 6; j++)
     {
-      if (vetor2[i] < vetor[j])
+      if (vetor[i] > vetor[j])
       {
-        aux = vetor[j];
-        vetor[j] = vetor2[i];
-        break;
+          aux = vetor[j];
+          vetor[j] = vetor[i];
+          vetor[i] = aux;
       }
     }
   }
-  for (i = 0; i < 5; i++)
-  {
-    printf("%d\n", vetor[i]);
-  }
-  fp = fopen("sudoku.txt","w");
+  // modo escrita
+  fp = fopen("top5.txt","w");
   for (i = 0; i < 5; i++)
   {
     fprintf(fp, "%d\n", vetor[i]);
