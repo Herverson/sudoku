d4ba205529624ad396a203195673643f569f317b
Herverson <herverson.sousa@gmail.com>
2017-07-04 11:25:01 -0300

    Quase pronto

--- sudoku.h
+++ sudoku.h
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
+
 #define FALSE 0
 #define TRUE 1
+struct rank
+{
+  char nome[50];
+  int segundos;
+};
+typedef struct rank Rank;
 
 int soluSudoku(int, int);
 void menu();
@@ -10,7 +18,9 @@ void geraMatriz(int);
 void printSudoku2();
 void jogarSudoku();
 void geraMatrizUsuario();
-void verificaSudoku();
+void rankMelhores();
+void top5(char *, int);
+int verificaSudoku();
 int mesmaColuna(int, int, int);
 int mesmaLinha(int, int, int);
 int mesmoQuadrado(int, int, int);
