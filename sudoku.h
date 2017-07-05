a1a74560009e103aea782d29eaa8682957f26abd
Herverson <herverson.sousa@gmail.com>
2017-07-05 02:01:49 -0300

    printSudoku

--- sudoku.h
+++ sudoku.h
@@ -14,11 +14,13 @@ typedef struct rank Rank;
 
 int soluSudoku(int, int);
 void menu();
+void adicionaQuant(int);
 void geraMatriz(int);
-void printSudoku2();
+void printSudoku();
 void jogarSudoku();
 void geraMatrizUsuario();
 void rankMelhores();
+void imprimeRank(char *);
 void top5(char *, int);
 int verificaSudoku();
 int mesmaColuna(int, int, int);
